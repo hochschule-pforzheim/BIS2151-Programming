666bdfa16c3542adeaa5ab1bb7e9e1b6f6807d23
wernerburkard <werner.burkard@hs-pforzheim.de>
2020-04-02 13:57:27 +0200

    Update Student.java

--- src/tutorial/Student.java
+++ src/tutorial/Student.java
@@ -6,45 +6,107 @@ package tutorial;
  */
 public class Student 
 { 
-    private String name;
+    private String prename;
     private String surname;
-    
-    public Student()
-    {
-        // this is an example for a default constructor.
-    }
-    public Student(String surname)
-    {
-        this.surname = surname;
-    }
-    
-    public Student(String name, String surname)
-    {
-        this.name = name;
-        this.surname = surname;
-    }
-    
-    public String getName()
-    {
-        return name;
+    private char state; // R=regular, I=internship, V=vacation, A=abroad
+    // current Semester 1-30: student is immatriculated
+    // currentSemester  100+ x : student concludes successfully in semster x
+    // curentSemester  -100- x : student failed after semester x
+    private int currentSemester;
+    private boolean european; // if it is an european citizen
+
+    public char getState() {
+        return state;
     }
 
-    public void setName(String name)
-    {
-        if(name.equals("John Jackson"))
-        {
-           this.name = name; 
+    public void setState(char state) {
+        this.state = state;
+    }
+
+    public int getCurrentSemester() {
+        return currentSemester;
+    }
+
+    public void setCurrentSemester(int currentSemester) {
+        this.currentSemester = currentSemester;
+    }
+
+    public boolean isEuropean() {
+        return european;
+    }
+
+    public void setEuropean(boolean european) {
+        this.european = european;
+    }
+
+    // Constructors
+    public Student() {
+        // this is the default constructor
+        this("Max", "Mustermann", 'R', (byte) 1, true);
+    }
+
+    public Student(String prename, String surname, char state, int currentSemester, boolean european) {
+        this.prename = prename;
+        this.surname = surname;
+        this.state = state;
+        this.currentSemester = currentSemester;
+        this.european = european;
+    }
+
+    // getter and setter methods
+    public String getPrename() {
+        return prename;
+    }
+
+    public void setPrename(String prename) {
+        if (prename.equals("John Jackson")) {
+            this.prename = prename;
         }
     }
 
-    public String getSurname()
-    {
+    public String getSurname() {
         return surname;
     }
 
-    public void setSurname(String surname)
-    {
+    public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    public void nextSemester() {
+        // switches the student to the next semester
+        if (state != 'V') {
+            ++currentSemester;
+        }
+    }
+
+    public void goInternship() {
+        state = 'I';
+        // another way:
+        this.setState('I');
+    }
+
+    public void successfulConclusion() {
+        if (currentSemester < 100 && currentSemester > 0) {
+            currentSemester = 100 + currentSemester;
+        }
+    }
+
+    public void failedConclusion() {
+        if (currentSemester < 100 && currentSemester > 0) {
+            currentSemester = -100 - currentSemester;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        String me;
+        me = prename+" "+surname+" (actual state "+state+") actual Semester:"+currentSemester;
+        if (isEuropean()){
+            me = me + " (European)";
+        } else{
+            me = me + " (from abroad)";
+        }
+        return me;
     }
 }
 
