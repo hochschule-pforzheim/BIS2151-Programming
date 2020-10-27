2bce0b8ae7227c68311c3723c4974c1402890398
ainzone <mail@ainz.one>
2020-10-27 16:56:06 +0100

    added attestation 1 task 4

---
+++ src/attestation1/SomeVariables.java
@@ -0,0 +1,21 @@
+package attestation1;
+
+/**
+ * Possible solution for attestation 1 task 4
+ *
+ * @author ainzone
+ * @version 1.0
+ */
+public class SomeVariables
+{
+
+    private double accountBalance;
+    private double articlePrice;
+    private String idCardNumber;
+    private double instalment;
+    private int matriculationNr;
+
+    /*
+     * It is also possible to use float instead of double!
+     */
+}
