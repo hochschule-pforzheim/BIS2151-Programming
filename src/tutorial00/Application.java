4f4c8df5e095191f6044ad647a2265e334d9fb21
JPfaefflin <jaschapf75@gmail.com>
2022-03-22 19:04:50 +0100

    added System.out.println()

--- src/tutorial00/Application.java
+++ src/tutorial00/Application.java
@@ -11,6 +11,7 @@ public class Application {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World!");
     }
 
 }
