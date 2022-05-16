e41dd4781eb2e9aeb7264a9a4bf6670953c4fe2f
JPfaefflin <jaschapf75@gmail.com>
2022-05-16 15:21:10 +0200

    added Singleton aswell as delete & update functions

--- src/tutorial05/RoomManager.java
+++ src/tutorial05/RoomManager.java
@@ -13,22 +13,72 @@ import java.util.ArrayList;
 public class RoomManager {
 
     private ArrayList<Room> rooms = new ArrayList<>();
+    private static RoomManager theRoomManager;
 
+    private RoomManager() {
+    }
+
+    public static RoomManager getInstance() {
+        if (theRoomManager == null) {
+            theRoomManager = new RoomManager();
+        }
+        return theRoomManager;
+    }
+
+    /**
+     *
+     * @param room der Raum, der zur Liste hinzugefügt werden soll
+     */
     public void addRoom(Room room) {
         rooms.add(room);
     }
 
+    /**
+     * Zeigt alle Räume an
+     *
+     * @return String mit Liste aller Räumen
+     */
     public String readRooms() {
         String output = "";
 
+        /*
         for (int i = 0; i < rooms.size(); i++) {
             output += rooms.get(i).toString() + "\n";
         }
-
+         */
         for (Room r : rooms) {
-            output += r.toString();
+            output += r.toString() +"\n";
         }
-
+        
         return output;
+    }
+
+    public void deleteRoomIndex(int index) {
+        rooms.remove(index); // nach index
+    }
+
+    public void deleteRoomID(int id) {
+        Room r = findRoom(id);
+        if (r != null) {
+            rooms.remove(r); // nach Objekt
+        }
+    }
+
+    public Room findRoom(int id) {
+        for (Room r : rooms) {
+            if (r.getRoomID() == id) {
+                return r;
+            }
+        }
+        return null;
+    }
+
+    public void updateDescription(int id, String input) {
+        Room r = findRoom(id);
+        System.out.println("Alte Description: " + r.getDescription());
+
+        if (r != null) {
+            r.setDescription(input);
+        }
     }
 }
