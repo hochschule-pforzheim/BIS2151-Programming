a0eee933ef8a57b9394f397cf61561db422e5943
Andreas R. Schneider <mail@ainz.one>
2023-10-09 23:02:47 +0200

    Added Unit 01 Examples

---
+++ src/hspf/unit01/ExampleArgs.java
@@ -0,0 +1,37 @@
+package hspf.unit01;
+
+/**
+ * Example class for args.
+ *
+ * @author Andreas R. Schneider
+ */
+public class ExampleArgs
+{
+
+    public static void main(String[] args)
+    {
+        // Check if there are enough command line arguments (name and age)
+        if (args.length < 2)
+        {
+            System.out.println("Usage: java Main <name> <age>");
+            return;
+        }
+
+        // Get name and age from command line arguments
+        String name = args[0];
+        int age = 0;
+
+        // Parse age from the second command line argument
+        try
+        {
+            age = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e)
+        {
+            System.out.println("Invalid age. Please enter a valid number.");
+            return;
+        }
+
+        // Display a message with the user's name and age
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+    }
+}
