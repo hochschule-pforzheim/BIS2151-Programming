b27620679f1c7976cfc1a361743fa8e4136fe2e0
Andreas R. Schneider <mail@ainz.one>
2023-11-13 08:47:06 +0100

    Create CodingSessionOne.java

---
+++ src/unit05/CodingSessionOne.java
@@ -0,0 +1,16 @@
+package unit05;
+
+/**
+ * Example Solution for Coding Session #1
+ *
+ * @author Andreas Schneider
+ */
+public class CodingSessionOne
+{
+    public void solution()
+    {
+        char[] words = {'H', 'E', 'L', 'L', 'O'};
+        words[4] = '0';
+        System.out.println(words);
+    }
+}
