aec814263c434d2ae944b36382424a0ce5d54bd6
Andreas R. Schneider <mail@ainz.one>
2023-11-20 17:04:09 +0100

    Added Bubble Sort Algorithm

---
+++ src/unit06/BubbleSort.java
@@ -0,0 +1,58 @@
+package unit06;
+
+/**
+ * Example class for a bubble sort implementation based on Nikita Tiwari
+ *
+ * @author Andreas Schneider
+ * @author Nikita Tiwari
+ */
+public class BubbleSort
+{
+
+    /**
+     * Method for running a bubble sort based on an Array "arr" and the array's
+     * length "n"
+     *
+     * @param arr
+     * @param size
+     */
+    public static void run(int arr[], int size)
+    {
+        int outer, inner, temp;
+        boolean swapped;
+        for (outer = 0; outer < size - 1; outer++) {
+            swapped = false;
+            for (inner = 0; inner < size - outer - 1; inner++) {
+                if (arr[inner] > arr[inner + 1]) {
+
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[inner];
+                    arr[inner] = arr[inner + 1];
+                    arr[inner + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped by inner loop, then break
+            if (swapped == false) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Method for printing the given array.
+     *
+     * @param arr
+     * @param size
+     */
+    public static void printArray(int arr[], int size)
+    {
+        int i;
+        for (i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
--- src/university/Lecture.java
+++ src/university/Lecture.java
@@ -15,7 +15,15 @@ public class Lecture
         // unit05.StudentStorage.run();
         // unit05.WrapperExample.run();
         // unit05.CodingSessionOne.run();
-        StudentManager.run();
-        StudentManager.printStudents();
+        // StudentManager.run();
+        // StudentManager.printStudents();
+
+        // Bubble Sort Section
+        int demoArray[] = {64, 34, 25, 12, 22, 11, 90};
+        int length = demoArray.length;
+        unit06.BubbleSort.run(demoArray, length);
+        System.out.println("Sorted array: ");
+        unit06.BubbleSort.printArray(demoArray, length);
+
     }
 }
