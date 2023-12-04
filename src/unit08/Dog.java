308a47713cfc0e57a99f55d977237d263542e385
Andreas R. Schneider <mail@ainz.one>
2023-12-04 17:38:31 +0100

    Added Interface Example

--- src/unit08/Dog.java
+++ src/unit08/Dog.java
@@ -5,7 +5,7 @@ package unit08;
  *
  * @author Andreas Schneider
  */
-public class Dog extends Canine
+public class Dog extends Canine implements Pet
 {
 
     /**
@@ -15,5 +15,17 @@ public class Dog extends Canine
     public void roam()
     {
         System.out.println("Roaming in a group!");
+    }
+
+    @Override
+    public void beFriendly()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void play()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
---
+++ src/unit08/Pet.java
@@ -0,0 +1,14 @@
+package unit08;
+
+/**
+ * Example Interface Pet
+ *
+ * @author Andreas Schneider
+ */
+public interface Pet
+{
+
+    public abstract void beFriendly();
+
+    public abstract void play();
+}
