03bb1255e6e602a42f1e10e82f1b98787af73f8a
Andreas R. Schneider <mail@ainz.one>
2023-12-08 22:10:59 +0100

    Update TestFormats.java

--- src/unit10/TestFormats.java
+++ src/unit10/TestFormats.java
@@ -1,5 +1,7 @@
 package unit10;
 
+import java.util.Date;
+
 /**
  * Number Format Example
  *
@@ -14,5 +16,16 @@ public class TestFormats
         System.out.println(first);
         String second = String.format("I have %,.2f bugs to fix.", 476578.09876);
         System.out.println(second);
+
+        // Combination
+        int one = 20456654;
+        double two = 199567890.248907;
+        String third = String.format("The rank is %,d out of %,.2f", one, two);
+
+        // Timestamps
+        String tOne = String.format("%tc", new Date());
+        System.out.println(tOne);
+        String tTwo = String.format("%tr", new Date());
+        System.out.println(tTwo);
     }
 }
