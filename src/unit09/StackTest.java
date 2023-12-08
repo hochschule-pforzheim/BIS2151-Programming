9924efa80f459722b282a6dd008e88adfb92d92a
Andreas R. Schneider <mail@ainz.one>
2023-12-08 19:16:12 +0100

    Create StackTest.java

---
+++ src/unit09/StackTest.java
@@ -0,0 +1,17 @@
+package unit09;
+
+import university.Student;
+
+/**
+ * Example Class for code of the stack
+ *
+ * @author Andreas Schneider
+ */
+public class StackTest
+{
+
+    public void go()
+    {
+        Student testStudent = new Student();
+    }
+}
