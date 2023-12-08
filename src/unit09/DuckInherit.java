192eec25107358a604573de3ca18ac2134da0f15
Andreas R. Schneider <mail@ainz.one>
2023-12-08 20:32:51 +0100

    Added StackRef and DuckInherit

---
+++ src/unit09/DuckInherit.java
@@ -0,0 +1,21 @@
+package unit09;
+
+import unit08.Animal;
+
+/**
+ * Example Duck class
+ *
+ * @author Andreas Schneider
+ */
+public class DuckInherit extends Animal
+{
+
+    int size;
+
+    public DuckInherit(int newSize)
+    {
+        super();
+        size = newSize;
+    }
+
+}
---
+++ src/unit09/StackRef.java
@@ -0,0 +1,20 @@
+package unit09;
+
+/**
+ * Example Stack Reference Class
+ *
+ * @author Andreas Schneider
+ */
+public class StackRef
+{
+
+    public void foof()
+    {
+        barf();
+    }
+
+    public void barf()
+    {
+        Duck d = new Duck();
+    }
+}
