f5be0decf1fe672c1c465432fcaeca76baa50d81
Andreas R. Schneider <mail@ainz.one>
2024-01-08 23:08:54 +0100

    Added greedy approach

---
+++ src/unit12/Activity.java
@@ -0,0 +1,23 @@
+package unit12;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Example class "Activity"
+ *
+ * @author Andreas Schneider
+ */
+class Activity
+{
+
+    int startTime;
+    int endTime;
+
+    public Activity(int startTime, int endTime)
+    {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
